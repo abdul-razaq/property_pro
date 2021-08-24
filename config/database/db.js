fab8f4be3a1b624723af50cb3e27dec4a20c11cb
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-08-24 19:34:41 +0100

    defined the database connection

--- config/database/db.js
+++ config/database/db.js
@@ -1,9 +1,43 @@
-import dotenv from 'dotenv';
-import { Pool } from 'node-postgres';
+import dotenv from "dotenv";
+import { Pool } from "node-postgres";
 
 dotenv.config();
 
+const connectionString =
+	process.env.APP_MODE === "DEVELOPMENT"
+		? process.env.DEV_DB_CONN_STRING
+		: PROD_DB_CONN_STRING;
+
 const pool = new Pool({
-  connectionString: process.env.DB_CONN_STRING,
+	connectionString,
 });
 
+pool.on("error", (err, _) => {
+	console.error("Unexpected error on idle client!", err);
+	process.exit(-1);
+});
+
+const dbConnection = {
+	/**
+	 * Connect to the database.
+	 * @constructor
+	 * @param {*} queryString - The passed in SQL query to execute.
+	 * @param {*} queryParams - The passed in query parameter values to pass to the query.
+	 */
+	queryDB: async (queryString, queryParams) => {
+		try {
+			const client = await pool.connect();
+			try {
+				return await client.query(queryString, queryParams);
+			} finally {
+				client.release();
+			}
+		} catch (e) {
+			setImmediate(() => {
+				throw e;
+			});
+		}
+	},
+};
+
+export default dbConnection;
