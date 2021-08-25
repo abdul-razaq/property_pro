16190b90c38584bbd603b305ac07605bcef45130
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-08-25 22:25:01 +0100

    created UnImplemented error class

--- helpers/app_error.js
+++ helpers/app_error.js
@@ -1,10 +1,30 @@
-export default class AppError extends Error {
+import httpStatuses from "./http_statuses.js";
+
+/**
+ * @class
+ * 
+ * Error thrown when an error occurred in the Application.
+ */
+export class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
-    this.statusCode = statusCode ?? 500;
+    this.statusCode = statusCode ?? httpStatuses.statusInternalServerError;
     this.status = String(statusCode).startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
     Error.captureStackTrace(this, this.constructor);
   }
 }
- 
+
+/**
+ * @class
+ * 
+ * Error thrown when a concrete implementation of an abstract class isn't implemented.
+ */
+export class UnImplementedError extends AppError {
+	constructor(methodName) {
+		super(
+			`Missing concrete implementation of ${methodName}`,
+			httpStatuses.statusNotImplemented
+		);
+	}
+}
