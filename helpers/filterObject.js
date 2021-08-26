74ed1da3a6fec41a2ec316afec0260d648119fce
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-08-26 12:59:17 +0100

    created function to filter object"

---
+++ helpers/filterObject.js
@@ -0,0 +1,9 @@
+function filterObject(object, ...unwantedFields) {
+	const obj = { ...object };
+	for (let key of Object.keys(obj)) {
+		if (unwantedFields.includes(key)) {
+			delete obj[key];
+		}
+	}
+  return obj;
+}
