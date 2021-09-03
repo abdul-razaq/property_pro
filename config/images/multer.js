0c832d073818437ca21aacfec723c5de6d92c5cd
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-09-03 11:06:31 +0100

    added multer config

---
+++ config/images/multer.js
@@ -0,0 +1,31 @@
+import multer from "multer";
+
+import AppError from "../../helpers/app_error";
+import httpStatuses from "../../helpers/http_statuses";
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = function (req, file, callback) {
+	const acceptedExt = ["jpg", "jpeg", "png", "gif", "svg", "tiff"];
+	if (!acceptedExt.includes(file.mimetype.split("/")[1])) {
+		callback(
+			new AppError(
+				"invalid image type provided.",
+				httpStatuses.statusBadRequest
+			),
+			false
+		);
+	} else {
+		callback(null, true);
+	}
+};
+
+const upload = multer({
+	storage: multerStorage,
+	fileFilter: multerFilter,
+	limits: {
+		fileSize: 10485760,
+	},
+});
+
+export default upload.single("image");
